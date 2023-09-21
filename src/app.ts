8bc700cbd80b3c7a9be3c4c2915f813496bb54d6
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2023-09-21 22:31:06 +0600

    add all error in globalError handler

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+
+import cookieParser from 'cookie-parser';
+import router from './app/routes';
+
+import morgan from 'morgan';
+
+const app: Application = express();
+
+//  middleware
+app.use(cors());
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// morgan
+app.use(morgan('dev'));
+
+// All Routes in routes
+app.use('/api/v1', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Assignment-8 Book Catalog Server is Running');
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
---
+++ src/errors/handleClientError.ts
@@ -0,0 +1,34 @@
+import { Prisma } from '@prisma/client';
+import { IGenericErrorMessage } from '../interfaces/error';
+
+const handleClientError = (error: Prisma.PrismaClientKnownRequestError) => {
+  let errors: IGenericErrorMessage[] = [];
+  let message = '';
+  const statusCode = 400;
+
+  if (error.code === 'P2025') {
+    message = (error.meta?.cause as string) || 'Record not found!';
+    errors = [
+      {
+        path: '',
+        message: message ? message : 'something went wrong',
+      },
+    ];
+  } else if (error.code === 'P2003') {
+    message = (error.meta?.field_name as string) || 'Invalid Foreign Key | ID';
+    errors = [
+      {
+        path: '',
+        message: message ? message : 'something went wrong',
+      },
+    ];
+  }
+
+  return {
+    statusCode,
+    message,
+    errorMessages: errors,
+  };
+};
+
+export default handleClientError;
